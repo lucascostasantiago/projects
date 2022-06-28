c9ff55b2e4d8e7eda082f5945d104a5a4534ded7
lucascostasantiago <lcscostasantiago@gmail.com>
2022-06-27 22:02:52 -0300

    feat: changing view privacy and changing project name to home in header.

--- Projects/Controllers/HomeController.cs
+++ Projects/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace Projects.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Functionality()
         {
             return View();
         }
